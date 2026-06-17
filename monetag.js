30e251b02c9bbbc3e16332db620a9b34a9613d76
Anynomous <156112254+yosintv2@users.noreply.github.com>
2026-06-17 21:21:35 +0900

    Update monetag.js

--- monetag.js
+++ monetag.js
@@ -1,24 +1,7 @@
-// Monetag Popup
-setTimeout(() => {
-    (function(s, u, z, p) {
-        s.src = u;
-        s.setAttribute('data-zone', z);
-        p.appendChild(s);
-    })(
-        document.createElement('script'),
-        'https://bvtpk.com/tag.min.js',
-        7463304,
-        document.body || document.documentElement
-    );
-}, 8000);
-
-// Monetag Vignette
-setTimeout(() => {
-    (function(s) {
-        s.dataset.zone = '7496520';
-        s.src = 'https://dd133.com/vignette.min.js';
-    })(
-        (document.body || document.documentElement)
-            .appendChild(document.createElement('script'))
-    );
-}, 2000);
+// Monetag
+        setTimeout(() => {
+            (function(s, u, z, p) {
+                s.src = u; s.setAttribute('data-zone', z);
+                p.appendChild(s);
+            })(document.createElement('script'), 'https://bvtpk.com/tag.min.js', 7463304, document.body || document.documentElement);
+        }, 8000);
