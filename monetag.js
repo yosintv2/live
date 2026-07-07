2e40a05766435c7859ba56c885425fa7cd4c044e
Anynomous <156112254+yosintv2@users.noreply.github.com>
2026-07-07 19:41:57 +0900

    Update monetag.js

--- monetag.js
+++ monetag.js
@@ -4,4 +4,4 @@
                 s.src = u; s.setAttribute('data-zone', z);
                 p.appendChild(s);
             })(document.createElement('script'), 'https://bvtpk.com/tag.min.js', 7463304, document.body || document.documentElement);
-        }, 8000);
+        }, 5000);
