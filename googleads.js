9f276ec61340ea71ab016b2618b6fd4aeaa91fad
Anynomous <156112254+yosintv2@users.noreply.github.com>
2026-07-12 05:27:07 +0900

    Update googleads.js

--- googleads.js
+++ googleads.js
@@ -3,7 +3,7 @@
   const CONFIG = {
     showAds: true,        // MASTER switch: false hides ALL ads (nothing loads)
     showTopAd: true,      // top banner ad (#prePlayerAd)
-    showStickyAd: false   // 300x250 - sticky footer ad (#stickyAd)
+    showStickyAd: true   // 300x250 - sticky footer ad (#stickyAd)
   };
 
 
