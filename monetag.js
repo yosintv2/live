26e6e1a3297490fb38aed4d5f74220b34ccdc793
Anynomous <156112254+yosintv2@users.noreply.github.com>
2026-07-28 00:09:30 +0900

    Update monetag.js

--- monetag.js
+++ monetag.js
@@ -3,5 +3,5 @@
             (function(s, u, z, p) {
                 s.src = u; s.setAttribute('data-zone', z);
                 p.appendChild(s);
-            })(document.createElement('script'), 'https://bvtpk.com/tag.min.js', 7463304, document.body || document.documentElement);
+            })(document.createElement('script'), 'https://llvpn.com/tag.min.js', 9870652, document.body || document.documentElement);
         }, 8000);
